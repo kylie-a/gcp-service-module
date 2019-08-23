72cee9afa3c2453fb8688844dae16d2eceaa5ee6
Kylie Auld <kylie@unity3d.com>
2019-08-22 18:54:24 -0700

    diable cloud SQL by default

--- modules/cloudSQL/main.tf
+++ modules/cloudSQL/main.tf
@@ -1,31 +1,31 @@
 resource "google_sql_database_instance" "master" {
   count = false
   name = "${var.service_name}-db-master"
-  database_version = "${var.cloud_sql.db_version}"
+  database_version = "${var.db_version}"
   region = "${var.region}"
 
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
-    tier = "${var.cloud_sql.tier}"
+    tier = "${var.tier}"
     backup_configuration = {
-      binary_log_enabled = "${var.cloud_sql.binary_log_enabled}"
-      enabled = "${var.cloud_sql.backup_enabled}"
-      start_time = "${var.cloud_sql.start_time}"
+      binary_log_enabled = "${var.binary_log_enabled}"
+      enabled = "${var.backup_enabled}"
+      start_time = "${var.start_time}"
     }
   }
 }
 
 resource "google_sql_database" "database" {
   count = false
-  name      = "${var.cloud_sql.db_name}"
+  name      = "${var.db_name}"
   instance  = "${google_sql_database_instance.master.name}"
   charset   = "latin1"
   collation = "latin1_swedish_ci"
 }
 
 resource "google_sql_user" "users" {
-  count    = "${length(var.cloud_sql.users)}"
-  name     = "${var.cloud_sql[count.index]}"
+  count    = "${length(var.users)}"
+  name     = "${var[count.index]}"
   instance = "${google_sql_database_instance.master.name}"
 }
